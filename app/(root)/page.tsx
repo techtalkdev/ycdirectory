868f42c0b13c290a40edb0005681401859632cda
Mike <binarybardcode@gmail.com>
2025-02-26 19:04:16 +0200

    Refactor Sanity client integration; implement live content fetching and enhance startup query with search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,14 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string}>}) {
-  const query = ( await searchParams ).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  const query = ( await searchParams ).query;
+  const params = { search: query  || null };
+  
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -36,6 +38,8 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
